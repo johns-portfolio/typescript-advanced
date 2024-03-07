15dcfb954491fac7beb702c4575fcac4b676da0c
Apisit Sianglert <john.apisits@gmail.com>
2024-03-07 08:11:59 +0700

    Generic type

---
+++ 2. intermediate level/02-generic-type.ts
@@ -0,0 +1,30 @@
+// type ApiResponse = {
+//   code: number
+//   data: any
+// }
+
+// const res: ApiResponse = {
+//     code: 200,
+//     data: 100_000
+// }
+
+// const res2: ApiResponse = {
+//     code: 200,
+//     data: true
+// }
+
+// // # Generic Type
+// type ApiResponse<DataType> = {
+//   code: number
+//   data: DataType
+// }
+
+// const res: ApiResponse<string> = {
+//   code: 200,
+//   data: 'hello'
+// }
+
+// const res2: ApiResponse<boolean> = {
+//   code: 200,
+//   data: true
+// }
